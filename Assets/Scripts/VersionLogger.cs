65ef352467189438dbb11e0fb48efdae53cbdef4
Beni <bucsy.beni@gmail.com>
2021-10-10 16:10:04 +0200

    test build

--- Assets/Scripts/VersionLogger.cs
+++ Assets/Scripts/VersionLogger.cs
@@ -7,7 +7,7 @@ public class VersionLogger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"App version: {Application.version}");
+        Debug.Log($"App version-> {Application.version}");
     }
 
     // Update is called once per frame
